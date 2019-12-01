7687718aed9c98d38a9377fc902e726b4de25b42
jlwang16 <jialinhaoyunqi@gmail.com>
2019-12-01 12:52:23 +0800

    add first commit
    Signed-off-by: jlwang16 <jialinhaoyunqi@gmail.com>

--- app/src/main/java/com/example/jialinwang/activitrylunch_test/MainActivity.java
+++ app/src/main/java/com/example/jialinwang/activitrylunch_test/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.jialinwang.activitrylunch_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     MyDialogFragment dialogFragment;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         dialogFragment = new MyDialogFragment();
         dialogFragment.show(getSupportFragmentManager(), "dialog");
+        Log.d(TAG, "Test 123");
     }
 }
